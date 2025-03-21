48e2a0fc3b672f908226da4431d840b79daec958
EbruEsra <test@192.168.1.102>
2025-03-21 16:50:14 +0300

    feat: new structure and progress of lessons add

---
+++ attachments/02-essentials/src/app/user/user.component.ts
@@ -0,0 +1,25 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { User } from '../models/user.model';
+
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  imports: [],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css'
+})
+export class UserComponent {
+  @Input({required:true}) user!: User;
+  @Input({required: true}) selected!: boolean
+  @Output() select = new EventEmitter<string>();
+
+  get imageSrc() {
+    return 'assets/users/' + this.user.avatar
+  }
+
+  onSelectUser() {
+    console.log('clicked')
+    this.select.emit(this.user.id)
+  }
+}
